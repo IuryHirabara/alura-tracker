d1bf593fc4764699ecd78d31cfea80c617b0af99
Iury Hirabara <dev.iuryhirabara@gmail.com>
2022-07-13 17:04:36 -0300

    Conclusão do curso

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }">
+    <div class="column is-one-quarter">
+      <BarraLateral @aoTemaAlterado="trocarTema"></BarraLateral>
+    </div>
+    <div class="column is-three-quarter conteudo">
+      <Acoes @aoRegistrarTarefa="registraTarefa"></Acoes>
+      <div class="lista">
+        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"></Tarefa>
+        <Box v-if="verficaLista">
+          Vazio
+        </Box>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Vue from 'vue'
+import BarraLateral from './components/BarraLateral.vue'
+import Acoes from './components/Acoes.vue'
+import Tarefa from './components/Tarefa.vue';
+import Box from './components/Box.vue';
+import ITarefa from './interfaces/ITarefa.vue';
 
 export default Vue.extend({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    BarraLateral,
+    Acoes,
+    Tarefa,
+    Box
+  },
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo: false
+    }
+  },
+  computed: {
+    verficaLista(): boolean {
+      return this.tarefas.length === 0
+    }
+  },
+  methods: {
+    registraTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa)
+    },
+    trocarTema(modoEscuroAtivado: boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivado
+    }
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.lista {
+  padding: 1.25rem
+}
+
+main {
+  --bg-primario: #FFF;
+  --texto-primario: #000
+}
+
+main.modo-escuro {
+  --bg-primario: #2B2D42;
+  --texto-primario: #DDD
+}
+
+.conteudo {
+  background-color: var(--bg-primario);
 }
 </style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,54 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="">
+        </h1>
+        <button class="button" @click="alterarTema">{{ textoBotao }}</button>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    name: 'BarraLateral',
+    data() {
+        return {
+            modoEscuroAtivado: false
+        }
+    },
+    emits: ["aoTemaAlterado"],
+    computed: {
+        textoBotao() {
+            if (this.modoEscuroAtivado) {
+                return "Desativar modo escuro"
+            }
+
+            return "Ativar modo escuro"
+        }
+    },
+    methods: {
+        alterarTema() {
+            this.modoEscuroAtivado = !this.modoEscuroAtivado
+            this.$emit("aoTemaAlterado", this.modoEscuroAtivado)
+        }
+    }
+})
+</script>
+
+<style scoped>
+header {
+    padding: 1rem;
+    background: #0d3b66;
+    width: 100%;
+    height: 100vh;
+    text-align: center;
+}
+
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
