869c9a412ccf0f2315666432e8cffae14dba6819
Iury Hirabara <dev.iuryhirabara@gmail.com>
2023-04-15 19:42:46 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/alura-tracker-build/'
+  publicPath: '/alura-tracker-site/'
 })
